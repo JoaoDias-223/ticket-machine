a639f6a55835df1fd3bad2b2ad593e12e0fdfefb
Antonio Muniz <antonio.gustavo_@live.com>
2022-09-20 19:41:37 -0300

    Update TicketMachine.java

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -52,6 +52,7 @@ public class TicketMachine {
         if (saldo < valor) {
             throw new SaldoInsuficienteException();
         }
+        saldo-= valor;
         String result = "*****************\n";
         result += "*** R$ " + saldo + ",00 ****\n";
         result += "*****************\n";
